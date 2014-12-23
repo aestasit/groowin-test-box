c41fb2aa54f92418b186262e743a52870cf24afb
tripleKS <skorenko@gmail.com>
2014-12-23 18:19:21 +0300

    Dropped dependent 7zip.install from chocopacks.bat

--- scripts/chocopacks.bat
+++ scripts/chocopacks.bat
@@ -1,6 +1,12 @@
-:: Ensure path in not null for newly open shell window
+:: Ensure variables are defined for new open ssh session
 set /p PATH=<C:\Windows\Temp\PATH
 
+set TEMP=C:\Windows\Temp
+
+set USERPROFILE=C:\Users\vagrant
+
+
 :: Install all the things
-cmd /c c:\ProgramData\chocolatey\bin\choco install 7zip.install
+:: cmd /c c:\ProgramData\chocolatey\bin\choco install 7zip.install
+
 cmd /c c:\ProgramData\chocolatey\bin\choco install windows-sdk-6.1
